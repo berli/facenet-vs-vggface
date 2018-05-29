dd996e66a58fa1c5f418286f2e3a8bc37a068016
berli <16645709@qq.com>
2018-05-29 21:37:29 +0800

    Create install_dev_list.sh

---
+++ install_dev_list.sh
@@ -0,0 +1,9 @@
+sudo apt-get install cmake git
+git clone https://github.com/tensorflow/tensorflow.git
+
+#for caffe
+sudo apt-get install  libleveldb-dev libsnappy-dev libopencv-dev libhdf5-serial-dev libatlas-base-dev libopenblas-dev libgflags-dev libgoogle-glog-dev liblmdb-dev
+sudo apt-get install --no-install-recommends libboost-all-dev 
+git clone  https://github.com/BVLC/caffe.git
+
+git clone https://github.com/opencv/opencv.git
