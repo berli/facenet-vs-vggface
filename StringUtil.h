85a2146c1756e01b1df0c007e389397233a5805f
berli <16645709@qq.com>
2018-06-07 20:01:58 +0800

    Update StringUtil.h

--- StringUtil.h
+++ StringUtil.h
@@ -1,3 +1,21 @@
+
+#include <memory>
+#include <iostream>
+#include <string>
+#include <cstdio>
+
+using namespace std; //Don't if you're in a header-file
+
+template<typename ... Args>
+string string_format( const std::string& format, Args ... args )
+{
+    size_t size = snprintf( nullptr, 0, format.c_str(), args ... ) + 1; // Extra space for '\0'
+    unique_ptr<char[]> buf( new char[ size ] ); 
+    snprintf( buf.get(), size, format.c_str(), args ... );
+    return string( buf.get(), buf.get() + size - 1 ); // We don't want the '\0' inside
+}
+//////////////////////////////////////////////////////////////
+
 #include <stdarg.h>  // For va_start, etc.
 #include <memory>    // For std::unique_ptr
 
