728fe6cc83d7a1de20133463d98828d3262818a5
berli <libo22@gmail.com>
2018-08-10 21:18:49 +0800

    modified:   shape.py

--- shape.py
+++ shape.py
@@ -1,17 +1,14 @@
-'''''******************************************************************
-    > File Name: shape.py
-    > Author: berli
-    > Mail: berli@tencent.com 
-    > Created Time: 2018年08月10日 星期五 16时22分29秒
- *****************************************************************'''
-
 #!/usr/bin/python
 # -*- coding: utf8 -*-)
 
 import tensorflow as tf
 
 t = tf.constant([[[1, 1, 1], [2, 2, 2]], [[3, 3, 3], [4, 4, 4]]])
+l = tf.constant([1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 4, 4])
 s = tf.shape(t)
+s1 = tf.shape(t)[2]
 
 with tf.Session() as sess:
     print (sess.run(s))
+    print (sess.run(s1))
+    print (sess.run(l))
