2e855299d581ce7b9ef6b10b47cf6c95811c2e26
berli <libo22@gmail.com>
2018-11-06 16:30:48 +0800

    modified:   douban.py 	new file:   wyy.png 	new file:   wyy1.png

--- douban.py
+++ douban.py
@@ -15,6 +15,7 @@ def get_article_link(url):
     bsObj = BeautifulSoup(res, 'html.parser')
     links = bsObj.find_all('a')
     articles = {}
+    kind = {}
     max = 0;
     for link in links:
         l = str(link.get('href'))
@@ -23,6 +24,10 @@ def get_article_link(url):
         #print 'type(l):',type(l)
         if( l.find('/ebook/') == 0):
             articles['https://read.douban.com/'+l]='1'
+        if( l.find('/kind/') == 0):
+            for bs in bsObj.find_all('a', {'href':l}, class_='title active'):
+                print 'kind_title:', bs.text
+                kind['https://read.douban.com'+l] = bs.text
         if( l.find('https://read.douban.com/column/') == 0):
             articles[l] = "1"
         elif( l.find('?start=') == 0):
@@ -35,7 +40,7 @@ def get_article_link(url):
                 print start
                 print l
 
-    return articles, max
+    return articles, max, kind
 
 def get_article(url):
 
@@ -55,11 +60,61 @@ def get_article(url):
             line +=','
             special = True;
 
+    #评分
+    flag = False
+    for read in  bsObj.find_all('span', class_='score'):
+        line += read.text
+        line +=','
+        print 'score:', read.text
+        flag = True;
+    if(len(line) > 0 and not flag):
+        line +='0,'
+
+    #评价人数
+    for read in  bsObj.find_all('span', class_='amount'):
+        print 'rate people:', read.text.split()[0]
+        line += read.text.split()[0]
+        line +=','
+    if( len(line)> 0 and len(bsObj.find_all('span', class_='amount')) == 0):
+        line +='0,'
+   
+   #定价
+    price = 0 
+    for read in  bsObj.find_all('span', class_='current-price-count'):
+        print 'price:', read.text.replace('￥', '')
+        if( read.text.find('免费') == -1):
+            price = float(read.text.replace('￥', ''))
+            print 'price:', price
+            if( price > 0.0 ):
+                line += read.text.replace('￥', '')
+                line +=','
+        else:
+            #line += read.text.replace('￥', '')
+            line +='0,'
+
+    #折扣价
+    discount = False;
+    for read in  bsObj.find_all('span', class_='discount-price current-price-count'):
+        print 'discount-price:', read.text.replace('￥', '')
+        line += read.text.replace('￥', '')
+        line +=','
+        discount = True;
+    #原价
+    if(not discount ):
+        for read in  bsObj.find_all('s', class_='original-price-count'):
+            print 'original-price:', read.text.replace('￥', '')
+            if( float(read.text.replace('￥', '')) > 0):
+                price = float(read.text.replace('￥', ''))
+                line += read.text.replace('￥', '')
+                line +=','
+    if(special):
+        line +='0,'
+
+    #阅读人数
     if( not special):
         for read in  bsObj.find_all('span', class_='read-count'):
             print 'read-count:', read.text.split()[0]
             line += read.text.split()[0];
-            line +=','
     else:
         #专栏
         special_count = 0;
@@ -68,30 +123,62 @@ def get_article(url):
             if(cnt > special_count):
                 special_count = cnt
         line += str(special_count)
-        line +=','
         print 'specail read-count:', special_count
 
-    flag = False
-    for read in  bsObj.find_all('span', class_='score'):
-        line += read.text
-        line +=','
-        print 'score:', read.text
-        flag = True;
-    if(len(line) > 0 and not flag):
-        line +=','
-
-    for read in  bsObj.find_all('span', class_='amount'):
-        print 'rating:', read.text.split()[0]
-        line += read.text.split()[0]
 
     return line
+
+def get_kind(kind, kind_title):
+
+    #url = "https://read.douban.com/kind/532?start=0&sort=hot&promotion_only=False&min_price=None&max_price=None&works_type=None"
+    url = "https://read.douban.com/kind/{0}?start=0&sort=hot&promotion_only=False&min_price=None&max_price=None&works_type=None"
+    get_url = "https://read.douban.com/kind/{0:d}?start={1:d}&sort=hot&promotion_only=False&min_price=None&max_price=None&works_type=None"
+    
+    links,max, _ = get_article_link(url.format(kind))
+    page_url =[]
+    
+    if( max > 20):
+        for i in range(20, max, 20):
+            cur_url = get_url.format(int(kind), i)
+            page_url.append(cur_url)
+            #print 'page_url:',page_url
+
+    now = time.time()
+    day = time.strftime("%Y-%m-%d", time.localtime(now))
+    title ='article_' +kind_title+'_' +day + '.txt'
+    print 'file:',title
+
+#sudo pip install BeautifulSoup4 lxml
+    f = open(title, 'w')
+    #爬取一个分类的文章
+    for url in page_url:
+        temp={}
+        #爬取一篇文章的内容
+        for article,v in links.items():
+            print 'article:',article
+            line = get_article(article)
+            temp[line] = '1'
+            print 'temp:',temp
+
+            #防止被封IP
+            stop = random.randint(2,10)
+            print 'sleep:',stop
+            time.sleep(stop)
+
+        for line,v in temp.items():
+            if( len(line) > 0):
+                f.write(line+'\n')
+                f.flush()
+        print 'get url:',url
+        links,max, _ = get_article_link(url)
+
 
 if __name__ == '__main__':
 
     url = "https://read.douban.com/kind/532?start=0&sort=hot&promotion_only=False&min_price=None&max_price=None&works_type=None"
     get_url = "https://read.douban.com/kind/532?start={0:d}&sort=hot&promotion_only=False&min_price=None&max_price=None&works_type=None"
     
-    links,max = get_article_link(url)
+    links,max, kind_url = get_article_link(url)
     page_url =[]
     
     if( max > 20):
@@ -101,32 +188,18 @@ if __name__ == '__main__':
             #print 'page_url:',page_url
 
 
-    now = time.time()
-    day = time.strftime("%Y-%m-%d", time.localtime(now))
-    title ='article_' + day + '.txt'
-
 #sudo pip install BeautifulSoup4 lxml
-    f = open(title, 'w')
     while True:
-        for url in page_url:
-            temp={}
-            for article,v in links.items():
-                print 'article:',article
-                line = get_article(article)
-                temp[line] = '1'
-                print 'temp:',temp
+        #爬取一个分类的文章
+        for url, v in kind_url.items():
+            pos = url.find('/kind/')
+            if( pos != -1):
+                pos1 = url.find('?', pos)
+            if( pos1 != -1):
+                kind = url[pos+len('/kind/'):pos1-1]
+            print 'current kind:',kind
 
-                #防止被封IP
-                stop = random.randint(2,10)
-                print 'sleep:',stop
-                time.sleep(stop)
-
-            for line,v in temp.items():
-                if( len(line) > 0):
-                    f.write(line+'\n')
-                    f.flush()
-            print 'get url:',url
-            links,max = get_article_link(url)
+        get_kind(kind, v);
 
         time.sleep(24*3600)
 
