f07582f1824df08f60312a6db19a7208b00cb2b4
berli <16645709@qq.com>
2018-12-05 20:49:48 +0800

    Update install_dev_list.sh

--- install_dev_list.sh
+++ install_dev_list.sh
@@ -37,3 +37,6 @@ cd ffmpeg-4.0/
 
 git clone https://github.com/opencv/opencv.git
 
+#for python
+sudo pip install --upgrade pip
+sudo pip install pandas numpy matplotlib matplotlib tensorflow sklearn requests
