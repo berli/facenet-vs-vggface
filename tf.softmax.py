dc239c608243cbf52afe5cc412f011e47ad78de7
berli <libo22@gmail.com>
2019-01-21 10:48:59 +0800

    modified:   tf.softmax.py

--- tf.softmax.py
+++ tf.softmax.py
@@ -4,6 +4,5 @@ import tensorflow as tf
 a = [1.0,2.0,3.0,4.0]
 b = [0.3,0.1,0.0]
 
-with tf.Session() as sess:
-    print(sess.run(tf.nn.softmax(a)))
-    print(sess.run(tf.nn.softmax(b)))
+print(tf.nn.softmax(a))
+print(tf.nn.softmax(b))
